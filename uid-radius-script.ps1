82f45c9b33796a49a5557d414df39cc42c91133d
cesanetwan <gareth.hill@cesa.catholic.edu.au>
2013-12-09 11:00:15 +1030

    Update uid-radius-script.ps1

--- uid-radius-script.ps1
+++ uid-radius-script.ps1
@@ -1,5 +1,5 @@
 param([string]$global:strEventUser, [string]$global:strCallingStation)
-$ErrorActionPreference = Stop
+$ErrorActionPreference = "Stop"
 
 $global:aClientIPs = @()
 $global:aExclusions = @()
